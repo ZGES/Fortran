8bab6d6c38cc33b9180b6322660d5ad50d789a4e
Piotr <piotrpassternak@gmail.com>
2019-06-03 13:55:29 +0000

    Few adjustments, now cosinus is more visible(total time increased from 1 to 7), frequency of sampling is doubled, last results are scaled by frequency, noise is made only by adding one random value not adding one and subtracting second, fixed typo in variable name, format2 bit modified and that's all

--- Zadanie2/src/noisermv.f90
+++ Zadanie2/src/noisermv.f90
@@ -13,39 +13,37 @@ program noisermv
     type(C_PTR) :: planr2c
     type(C_PTR) :: planc2r
 
-    real, parameter :: Pi = 4*atan(1.0)
-    integer, parameter :: frec = 1024 !frecuency of sampling
+    integer, parameter :: freq = 2048 !frecuency of sampling
     real :: stime = 0.0 !represents time when the sample was taken
-    real :: interval = 1/real(frec-1) !difference between two time measures
+    real :: interval = 7/real(freq-1) !difference between two time measures
 
     integer :: it !iterator
-    real :: rand1, rand2 !noises in our signal
+    real :: rand1 !noises in our signal
     character(:), allocatable :: format1
     character(:), allocatable :: format2
 
     format1 = "(F13.10,A1,F22.16)"
-    format2 = "(I3,A1,F20.16)"
+    format2 = "(I3,A1,F21.16)"
 
     !initialization
-    allocate(tSigArray(frec))
-    allocate(fSigArray(frec/2 + 1))
+    allocate(tSigArray(freq))
+    allocate(fSigArray(freq/2 + 1))
     open(unit = 11, file = "../res/cosSignal.txt", status = 'REPLACE')
     open(unit = 12, file = "../res/noisySignal.txt", status = 'REPLACE')
     open(unit = 13, file = "../res/noisyFftSignal.txt", status = 'REPLACE')
     open(unit = 14, file = "../res/clearedFftSignal.txt", status = 'REPLACE')
     open(unit = 15, file = "../res/clearedSignal.txt", status = 'REPLACE')
-    call srand(67731)
+    call srand(86456)
 
 
-    do it = 1, frec
+    do it = 1, freq
         !base signal
-        tSigArray(it) = cos(2*Pi*stime*200)
+        tSigArray(it) = cos(stime)
         write(unit = 11, fmt = format1) stime, " ", tSigArray(it)
 
         !noisy signal
         rand1 = rand(0)
-        rand2 = rand(0)
-        tSigArray(it) = tSigArray(it) + rand1 - rand2
+        tSigArray(it) = tSigArray(it) + rand1
         write(unit = 12, fmt = format1) stime, " ", tSigArray(it)
         
         stime = stime+interval
@@ -55,7 +53,7 @@ program noisermv
     planr2c = fftw_plan_dft_r2c_1d(size(tSigArray), tSigArray, fSigArray, FFTW_ESTIMATE+FFTW_UNALIGNED)
     call fftw_execute_dft_r2c(planr2c, tSigArray, fSigArray)
 
-    do it = 1, frec/2+1
+    do it = 1, freq/2+1
         !noisy fft
         write(unit = 13, fmt = format2) it, " ", abs(fSigArray(it))
 
@@ -67,13 +65,13 @@ program noisermv
     end do
 
     !executing inverse fft on cleared fft 
-    planc2r = fftw_plan_dft_c2r_1d(size(fSigArray), fSigArray, tSigArray, FFTW_ESTIMATE+FFTW_UNALIGNED)
+    planc2r = fftw_plan_dft_c2r_1d(size(tSigArray), fSigArray, tSigArray, FFTW_ESTIMATE+FFTW_UNALIGNED)
     call fftw_execute_dft_c2r(planc2r, fSigArray, tSigArray)
 
     stime = 0.0
-    do it = 1, frec
+    do it = 1, freq
         !cleared signal
-        write(unit = 15, fmt = format1) stime, " ", tSigArray(it)
+        write(unit = 15, fmt = format1) stime, " ", tSigArray(it)/freq
         stime = stime+interval
     end do
 
