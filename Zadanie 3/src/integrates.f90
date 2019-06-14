d4e12cf0a53dee41b6f6b828bdc5bac082d20cfb
Piotr <piotrpassternak@gmail.com>
2019-06-14 18:45:08 +0000

    Move interface from main, fixed integrate functions

--- Zadanie 3/src/integrates.f90
+++ Zadanie 3/src/integrates.f90
@@ -9,6 +9,18 @@ module integrates
         end function fun_int
     end interface
 
+    interface
+        function integrate(ibeg, iend, myfun, p) result(value)
+            import :: fun_int
+            implicit none
+            real(kind  = 8), intent(in) :: ibeg !begining of integration interval
+            real(kind  = 8), intent(in) :: iend !end of the integration interval
+            procedure(fun_int) :: myfun !function to be integrated
+            integer(kind = 4), intent(in) :: p !polymonial order
+            real(kind = 8) :: value !result of integration
+        end function integrate
+    end interface
+
     contains
         function rectangle_int(ibeg, iend, myfun, p) result(value)
             implicit none
@@ -22,11 +34,11 @@ module integrates
             real(kind = 8) :: imid !middle of the interval given by ibeg and iend
             real(kind = 8) :: midval !function value at imid
 
-            dx = iend-ibeg
+            dx = abs(iend-ibeg)
             imid = (ibeg+iend)/2
             midval = myfun(imid)
 
-            value = abs(dx*midval)
+            value = dx*midval
 
         end function rectangle_int
 
@@ -42,11 +54,11 @@ module integrates
             real(kind = 8) :: firstval !function value at ibeg
             real(kind = 8) :: secval !function value at iend
 
-            dx = iend-ibeg
+            dx = abs(iend-ibeg)
             firstval = myfun(ibeg)
             secval = myfun(iend)
 
-            value = abs(dx*(firstval+secval)/2)
+            value = dx*(firstval+secval)/2
 
         end function trapezoidal_int
 
